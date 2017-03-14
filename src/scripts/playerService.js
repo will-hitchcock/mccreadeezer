1104188258d486c083fd331387ec64f775da8488
Will Hitchcock <william.hitchcock@mrm-mccann.com>
2017-03-14 18:20:01 -0400

    update private variables with the _ prefix

--- src/scripts/playerService.js
+++ src/scripts/playerService.js
@@ -3,33 +3,31 @@ angular.module('playerService', ['dragdealer'])
 .factory('PlayerService', ['$rootScope', '$document', function($rootScope, $document) {
     var audioElement = $document[0].createElement('audio'); // <-- Magic trick here
     return {
-        audioElement: audioElement,
-        songList: [],
-        curSong: {},
-        curIndex: -1,
-        playing: false,
-        position: 0,
-        curPosition: 0,
-        scrollTimer: null,
+        _audioElement: audioElement,
+        _songList: [],
+        _curSong: {},
+        _curIndex: -1,
+        _playing: false,
+        _curPosition: 0,
+        _seeking: false,
 
         play: function(i) {
 
-            if(i === this.curIndex) {
+            if(i === this._curIndex) {
                 //do nothing if trying to play the current song or play/pause
                 this.playPause();
                 return;
             }
-            this.audioElement.src = this.songList[i].mp3;
+            this._audioElement.src = this._songList[i].mp3;
             this._setCurSong(i);
             this._startPlayBack();
         },
         playPause: function() {
-            console.log('playerService.playPause', this.curIndex);
-            if (this.curIndex < 0) {
-                this.curIndex = 0;
-                this._setCurSong(this.curIndex);
+            if (this._curIndex < 0) {
+                this._curIndex = 0;
+                this._setCurSong(this._curIndex);
             }
-            if (this.playing) {
+            if (this._playing) {
                 this._stopPlayBack()
             } else {
                 this._startPlayBack();
@@ -37,55 +35,60 @@ angular.module('playerService', ['dragdealer'])
         },
         next: function() {
             //implement next
-            this.play(this.curIndex + 1);
+            this.play(this._curIndex + 1);
         },
         prev: function() {
             //implement prev
-            this.play(this.curIndex - 1);
+            this.play(this._curIndex - 1);
         },
         seek: function(seekTo) {
             //implement seek
-            this.audioElement.currentTime = seekTo;
+            this._audioElement.currentTime = seekTo;
         },
         init: function(data) {
             //init
             var self = this;
-            this.songList = angular.copy(data.songList);
+            this._songList = angular.copy(data.songList);
             
-            this.curSong = angular.copy(data.songList[0]);
+            this._curSong = angular.copy(data.songList[0]);
             
-            this.audioElement.ontimeupdate = function() {
-                self.updateCurPosition();
+            this._audioElement.ontimeupdate = function() {
+                if(!self._seeking){
+                    self.updateCurPosition();
+                }
             };
 
-            this.audioElement.onended = function() {
+            this._audioElement.onended = function() {
                 self.next();
             }
         },
 
 
         _setCurSong: function(i) {
-            this.curIndex = i;
-            this.curSong = angular.copy(this.songList[i]);
-            this.audioElement.src = this.songList[i].mp3;
+            this._curIndex = i;
+            this._curSong = angular.copy(this._songList[i]);
+            this._audioElement.src = this._songList[i].mp3;
         },
         _startPlayBack: function() {
-            this.playing = true;
-            this.audioElement.play(); //  <-- Thats all you need
+            this._playing = true;
+            this._audioElement.play(); //  <-- Thats all you need
 
         },
         _stopPlayBack: function() {
-            this.playing = false;
-            this.audioElement.pause(); //  <-- Thats all you need
+            this._playing = false;
+            this._audioElement.pause(); //  <-- Thats all you need
 
-            console.log(this.curPosition, this.audioElement.duration);
         },
-        updateCurPosition: function() {
-            if(this.audioElement.duration){
-                this.curPosition = (this.audioElement.currentTime / this.audioElement.duration)*100;
-                $rootScope.$apply();
-            }
-            //console.log(this.curPosition, this.audioElement.currentTime, this.audioElement.duration);
+        // stops the playing progress from being updated automatically
+        stopProgressBar: function() {
+            this._seeking = true;
+        },
+        resumeProgressBar: function() {
+            this._seeking = false;
+        },
+        updateCurPosition: function(num) {
+            this._curPosition = (this._audioElement.currentTime / this._audioElement.duration)*100;
+            $rootScope.$apply();
         }
     }
 }]);
